f907c8ba3335ba9d5e6bb145e7d30750be27b808
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-11-04 19:25:06 +0530

    Search input and button - movie-app

--- movieapp/components/Movie.js
+++ movieapp/components/Movie.js
@@ -1,12 +1,49 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
 
 const Movie = () => {
+  const [searchText, setSearchText] = useState("");
+  //   input change function
+  const handleInputChange = (inputText) => {
+    setSearchText(inputText);
+  };
+
+  //   handle button click
+  const handleSearchBtn = () => [console.log(searchText)];
   return (
-    <View>
-      <Text>Movie</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="find a movie"
+        value={searchText}
+        onChangeText={handleInputChange}
+      />
+      <View style={styles.searchBtn}>
+        <Button title="Search Movie" onPress={handleSearchBtn} />
+      </View>
     </View>
   );
 };
 
 export default Movie;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchInput: {
+    width: "75%",
+    height: "100",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  searchBtn: {
+    padding: 10,
+    width: "50%",
+    marginTop: 20,
+  },
+});
